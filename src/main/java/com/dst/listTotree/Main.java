1fac97c0ad7e1c731eee6dfbed98eca9200352de
姜广康 <602757148@qq.com>
2019-08-07 21:42:46 +0800

    修正

--- src/main/java/com/dst/listTotree/Main.java
+++ src/main/java/com/dst/listTotree/Main.java
@@ -41,7 +41,7 @@ public class Main {
         menu = new Menu("7", "0", "name4", "url4");
         list.add(menu);
 
-
+        System.out.println();
 
 //        System.out.println(JSONArray.fromObject(TreeUtil.getTree(list)));
         System.out.println(JSONArray.fromObject(TreeUtil.getTreeState(list,list1)));
