68cda443d1f9d13ffccff4da9bafaf679b7de31b
姜广康 <602757148@qq.com>
2019-08-07 21:40:08 +0800

    修正

--- src/main/java/com/dst/listTotree/Main.java
+++ src/main/java/com/dst/listTotree/Main.java
@@ -43,6 +43,7 @@ public class Main {
 
 
         System.out.println();
+        System.out.println();
 
 //        System.out.println(JSONArray.fromObject(TreeUtil.getTree(list)));
         System.out.println(JSONArray.fromObject(TreeUtil.getTreeState(list,list1)));
