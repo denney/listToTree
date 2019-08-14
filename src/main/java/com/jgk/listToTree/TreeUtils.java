cae8882dc2ca0cea02fb28943ec06b1beb5f020d
姜广康 <602757148@qq.com>
2019-08-14 08:47:24 +0800

    ...

--- src/main/java/com/jgk/listToTree/TreeUtils.java
+++ src/main/java/com/jgk/listToTree/TreeUtils.java
@@ -22,47 +22,11 @@ public class TreeUtils {
      */
     public static <T> JSONArray listToTreeJsonNew(String parentIdProperty, String idProperty, List<T> originList) {
         JSONObject object = new JSONObject();
-        object.put("nodes", new JSONArray());
 
-        try {
-            for (T node : originList) {
-                String nodeJson = JSONObject.toJSONString(node);
-                JSONObject jsonObject = JSONObject.parseObject(nodeJson);
-                JSONArray nodes = new JSONArray();
-                for (T temp : originList) {
-                    //获得temp父节点
-                    String tempJson = JSONObject.toJSONString(temp);
-                    JSONObject tempJsonObject = JSONObject.parseObject(tempJson);
 
-                    Field fpid = null;
-                    fpid = temp.getClass().getDeclaredField(parentIdProperty);
-                    fpid.setAccessible(true);
-                    String pid;
-                    if ("int".equals(fpid.getType().getName())) {
-                        pid = fpid.get(temp) + "";
-                    } else {
-                        pid = (String) fpid.get(temp);
-                    }
-                    if (StringUtils.isEmpty(pid) || "0".equals(pid) || "-1".equals(pid)) {
-                        object.getJSONArray("nodes").add(jsonObject);
-                    } else {
-                        //获得node节点id
-                        //id判断为空
-                        Field fid = node.getClass().getDeclaredField(idProperty);
-                        fid.setAccessible(true);
-                        String id = fid.get(node) + "";
-                        if (StringUtils.isNotEmpty(pid) && id.equals(pid)) {
-                            nodes.add(tempJsonObject);
-                        }
-                    }
-                }
-                jsonObject.put("nodes", nodes);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         return object.getJSONArray("nodes");
     }
+
 
 
     /**
